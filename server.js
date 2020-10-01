fd59eb57b14bca168aafdbcbf47624c70925ee03
rjmchi <robert@rjmprogramming.com>
2020-10-01 16:42:16 -0500

    adds cors

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const db = require("./models");
 const PORT = process.env.port || 3000;
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 const router = require("./routes/api");
 
